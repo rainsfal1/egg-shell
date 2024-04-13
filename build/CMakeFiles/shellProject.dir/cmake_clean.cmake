01f04b986edf185b9ed9ac6d060a2d94862cc6b8
RainsFall <59012139+rainsfal1@users.noreply.github.com>
2024-04-13 22:34:15 +0500

    Add files via upload

---
+++ build/CMakeFiles/shellProject.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shellProject.dir/src/commands/commands.c.o"
+  "CMakeFiles/shellProject.dir/src/commands/commands.c.o.d"
+  "CMakeFiles/shellProject.dir/src/main.c.o"
+  "CMakeFiles/shellProject.dir/src/main.c.o.d"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/cleanup.c.o"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/cleanup.c.o.d"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/displayPrompt.c.o"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/displayPrompt.c.o.d"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/executeCommand.c.o"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/executeCommand.c.o.d"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/parseCommand.c.o"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/parseCommand.c.o.d"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/readCommand.c.o"
+  "CMakeFiles/shellProject.dir/src/utils/mainloop/readCommand.c.o.d"
+  "CMakeFiles/shellProject.dir/src/utils/miscellaneous/eggsplay.c.o"
+  "CMakeFiles/shellProject.dir/src/utils/miscellaneous/eggsplay.c.o.d"
+  "CMakeFiles/shellProject.dir/src/utils/miscellaneous/initialize.c.o"
+  "CMakeFiles/shellProject.dir/src/utils/miscellaneous/initialize.c.o.d"
+  "CMakeFiles/shellProject.dir/src/utils/miscellaneous/shutdown.c.o"
+  "CMakeFiles/shellProject.dir/src/utils/miscellaneous/shutdown.c.o.d"
+  "shellProject"
+  "shellProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shellProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/shellProject.dir/src/commands/tictactoe/tictactoe.c.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/shellProject.dir/src/commands/tictactoe/tictactoe.c.o: \
+  /Users/macbook/projectShell/src/commands/tictactoe/tictactoe.c
